a7ffdb4009f73e3117e81dfb6b7ddc8b67941afd
Bob Jamieson <bamieson@gmail.com>
2022-08-16 23:04:23 +1000

    First commit. Testing.

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import MainNav from './MainNav'
+ 
+const Header = () => {
+
+
+
+    return (
+        <div>
+            <div className='Header-container'>
+                <img src='./logo.png' className='App-logo' alt='logo'
+                 />
+            </div>
+            
+            <div className='Nav-container'>
+                <div className='Subtext-container'>
+                    <h3>{'<Front End Developer />'}</h3>
+                </div>
+                <MainNav />            
+            </div>
+        </div> 
+    )
+}
+
+export default Header
---
+++ src/components/MainNav.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const MainNav = () => {
+
+    const [burgerActive, setBurger] = useState(false);
+
+
+    const burgerClick = () => {
+        setBurger(!burgerActive)
+    }
+
+    return (
+        
+        <div>
+            <button 
+                className={burgerActive ? "hamburger hamburger--squeeze is-active" : "hamburger hamburger--squeeze"}
+                onClick={burgerClick}
+                type="button">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </button>
+
+            { burgerActive ? (
+                    <nav>
+                        <li>Work</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Resume</li>
+                    </nav>)
+                : null
+            }
+         </div>
+    )
+}
+
+export default MainNav
